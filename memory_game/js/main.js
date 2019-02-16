d7a534e9746a635cf7cd5c1041362e3993424452
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-02-15 18:23:00 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTWo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped  " + cardThree);	
+var cards = ["Queen","Queen","King","King"];
+var cardsInPlay = [];
+var cardOne = null;
+var cardTwo = null;
+
+cardOne = cards[3];
+cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay.length === 2)
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert ("You got a match!");
+	else
+		alert ("Match not found, try again!");
+
