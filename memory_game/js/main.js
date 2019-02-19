cfebf37c4de0b0afca360cb9ae704ae900cbf247
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2019-02-18 19:05:43 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-var cards = ["Queen","Queen","King","King"];
+var cards = [{
+	rank:"queen",
+	suit:"hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}]
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,8 +31,10 @@ var checkForMatch = function(){
 }
 var flipCard = function(cardId){
 
-	console.log("User flipped a " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped a " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2)
 		checkForMatch();
